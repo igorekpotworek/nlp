73345e5e755c28c0dc2035c8a2a4d7566f3e7a59
igorekpotworek <igipopko@gmail.com>
2015-03-14 23:12:12 +0100

    initial commit

---
+++ nlp/src/main/java/nlp/Artykul.java
@@ -0,0 +1,38 @@
+package nlp;
+
+public class Artykul {
+	private String tytul;
+	private String wstep;
+	private String tekst;
+
+	public String getTytul() {
+		return tytul;
+	}
+
+	public void setTytul(String tytul) {
+		this.tytul = tytul;
+	}
+
+	public String getWstep() {
+		return wstep;
+	}
+
+	public void setWstep(String wstep) {
+		this.wstep = wstep;
+	}
+
+	public String getTekst() {
+		return tekst;
+	}
+
+	public void setTekst(String tekst) {
+		this.tekst = tekst;
+	}
+
+	@Override
+	public String toString() {
+		return "Artykul [tytul=" + tytul + ", wstep=" + wstep + ", tekst="
+				+ tekst + "]";
+	}
+
+}
